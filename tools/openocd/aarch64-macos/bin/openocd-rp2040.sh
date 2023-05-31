aa5d06664ed9d254474519ae3a2ad707d52f7c0a
Matt Knight <mattnite@proton.me>
2023-05-31 07:46:45 -0700

    readme and x86_64-macos openocd

--- tools/openocd/aarch64-macos/bin/openocd-rp2040.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-BIN_DIR=$(realpath $(dirname $0))
-SCRIPTS_DIR=$(realpath ${BIN_DIR}/../share/openocd/scripts)
-
-$BIN_DIR/openocd -s $SCRIPTS_DIR -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000"
